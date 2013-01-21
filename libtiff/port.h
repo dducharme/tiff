751c5fad355ab81628c8f51cd1236bbb9be97659
Dale Ducharme <dducharme@rim.com>
2013-01-21 18:12:08 -0500

    Initial commit of the tiff library.

---
+++ libtiff/port.h
@@ -0,0 +1,32 @@
+/*
+ * Warning, this file was automatically created by the TIFF configure script
+ * VERSION:	 v3.6.1
+ * RELEASE:   
+ * DATE:	 Thu Aug 9 06:55:27 EST 2012
+ * TARGET:	 i686-pc-linux-gnu
+ * CCOMPILER:	 /usr/bin/gcc-4.4.3-r2 p1.2' 4.4.3-r2 p1.2)
+ */
+#ifndef _PORT_
+#define _PORT_ 1
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <sys/types.h>
+#define HOST_FILLORDER FILLORDER_MSB2LSB
+#define HOST_BIGENDIAN	0
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+typedef double dblparam_t;
+#ifdef __STRICT_ANSI__
+#define	INLINE	__inline__
+#else
+#define	INLINE	inline
+#endif
+#define GLOBALDATA(TYPE,NAME)	extern TYPE NAME
+#ifdef __cplusplus
+}
+#endif
+#endif
